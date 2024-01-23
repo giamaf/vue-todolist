e78257066a81f479e7c17aa892b7995901bfb157
giamaf <gianluca.maffucci@hotmail.com>
2024-01-23 15:46:09 +0100

    added loop's print

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const { createApp } = Vue;
 const myApp = createApp({
     name: 'To Do List',
     data() {
-        return [
-            { id: 1, done: false, text: 'Fare la spesa' },
-            { id: 2, done: true, text: 'Lavare i capelli' },
-            { id: 3, done: false, text: 'Comprare una marca da bollo' },
-            { id: 4, done: false, text: 'Aggiornare il PC' }
-        ]
+        return {
+            tasks: [
+                { id: 1, done: false, text: 'Fare la spesa' },
+                { id: 2, done: true, text: 'Lavare i capelli' },
+                { id: 3, done: false, text: 'Comprare una marca da bollo' },
+                { id: 4, done: false, text: 'Aggiornare il PC' }
+            ]
+        }
     }
 })
 
