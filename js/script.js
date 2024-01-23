345b3416fbee927645692b67a4517a73677ef16d
giamaf <gianluca.maffucci@hotmail.com>
2024-01-23 17:52:08 +0100

    added add/remove task's logic

--- js/script.js
+++ js/script.js
@@ -11,7 +11,36 @@ const myApp = createApp({
                 { id: 2, done: true, text: 'Lavare i capelli' },
                 { id: 3, done: false, text: 'Comprare una marca da bollo' },
                 { id: 4, done: false, text: 'Aggiornare il PC' }
-            ]
+            ],
+
+            // ** Variabile importante per leggere i dati inseriti dall'utente
+            newTask: ''
+        }
+    },
+    methods: {
+
+        // # Funzione per cancellare un task. Passiamo un parametro "id" per individuare quale task vuole eliminare l'utente e riassegnamo il task originale
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => id !== task.id)
+        },
+
+        // # Funzione per creare un task. 
+        addTask() {
+            // ** Creo il nuovo oggetto da inserire nell'array originale
+            const newTask = {
+                // L'id è creato con una stringa data univoca
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTask
+            }
+
+            // ** Se c'è del testo inserisco l'oggetto nell'array
+            if (newTask.text) {
+                this.tasks.push(newTask)
+            }
+
+            // ** Svuoto il campo in pagina
+            this.newTask = ''
         }
     }
 })
