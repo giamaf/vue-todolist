88ae0f88feaec7f5c3cb107a66bcb74f38454be7
giamaf <gianluca.maffucci@hotmail.com>
2024-01-23 15:36:09 +0100

    js_set vue's data

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-console.log('JS OK');
+console.log('Vue OK', Vue);
 
+const { createApp } = Vue;
+
+const myApp = createApp({
+    name: 'To Do List',
+    data() {
+        return [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ]
+    }
+})
+
+myApp.mount('#root');
