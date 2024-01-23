53d09f9ff2e01033e5d84a32e5ee4c7fd2c22b4e
giamaf <gianluca.maffucci@hotmail.com>
2024-01-23 19:17:48 +0100

    added check button's toggle

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ const myApp = createApp({
         return {
             tasks: [
                 { id: 1, done: false, text: 'Fare la spesa' },
-                { id: 2, done: true, text: 'Lavare i capelli' },
+                { id: 2, done: false, text: 'Lavare i capelli' },
                 { id: 3, done: false, text: 'Comprare una marca da bollo' },
                 { id: 4, done: false, text: 'Aggiornare il PC' }
             ],
 
             // ** Variabile importante per leggere i dati inseriti dall'utente
-            newTask: ''
+            newTask: '',
         }
     },
     methods: {
@@ -24,8 +24,9 @@ const myApp = createApp({
             this.tasks = this.tasks.filter(task => id !== task.id)
         },
 
-        // # Funzione per creare un task. 
+        // # Funzione per creare un task. Non passo parametri perchè devo solo leggere. In lettura ho già la variabile "newTask"
         addTask() {
+
             // ** Creo il nuovo oggetto da inserire nell'array originale
             const newTask = {
                 // L'id è creato con una stringa data univoca
@@ -36,12 +37,21 @@ const myApp = createApp({
 
             // ** Se c'è del testo inserisco l'oggetto nell'array
             if (newTask.text) {
-                this.tasks.push(newTask)
+                this.tasks.push(newTask);
             }
 
             // ** Svuoto il campo in pagina
-            this.newTask = ''
-        }
+            this.newTask = '';
+        },
+
+        // # Funzione per toggolare la barra sul task
+        toggleIsDone(id) {
+            this.tasks.forEach(task => {
+                if (id === task.id) {
+                    task.done = !task.done
+                }
+            })
+        },
     }
 })
 
